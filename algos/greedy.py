1048d4a86f9aa31f8f53cb5eb0e0b733b30ac7f9
Utkarsh <utkarshboom77@gmail.com>
2023-04-20 01:33:35 +0800

    Greedy Algo Implemented

--- algos/greedy.py
+++ algos/greedy.py
@@ -1,53 +1,87 @@
-import numpy as np
- 
-# Function to find the minimum
-# cost path for all the paths
-def solver_greedy(graph: np.ndarray):
-    """
-    in:
-    @ tsp: np.ndarray: the NxN distance matrix
-    """
-    r, c = len(graph), len(graph[0])
-    i, j, total, counter = 0, 0, 0, 1
-    min_dist = 100 # Can just set an arbritarily large number since max distance between 2 points is just 1.4ish
-    visited = {x: 0 for x in range(r)}
-    
-    # Start from first node
-    visited[0] = 1
-    route = [0 for x in range(r+1)]
-    route[0] = 1
- 
-    # Traverse the adjacency
-    # matrix graph[][]
-    while i < r and j < c:
-        # Corner of matrix
-        if counter > (c-1):
-            break
- 
-        # If this path is unvisited then
-        # and if the cost is less then
-        # update the cost
-        if (j != i) and (visited[j] == 0):
-            if graph[i][j] < min_dist:
-                min_dist = graph[i][j]
-                route[counter] = j + 1
- 
-        j += 1
+def distance(a,b):
+    return ((a[0] - b[0])**2 + (a[1] - b[1])**2 )**(0.5)
 
-        # Check all paths from the
-        # ith indexed city
-        if j == r:
-            total += min_dist
-            min_dist = 100
-            visited[route[counter] - 1] = 1
-            j = 0
-            i = route[counter] - 1
+class Graph:
+    def __init__(self, cities):
+        self.cities = cities
+        self.length = len(cities)
+        self.graph = []
+        for i in range(self.length):
+            for j in range(i):
+                self.graph.append([i, j, distance(cities[i], cities[j])])
+                
+
+    def search(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.search(parent, parent[i])
+ 
+    def apply_union(self, parent, rank, x, y):
+        xroot = self.search(parent, x)
+        yroot = self.search(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+ 
+  
+    def tsp(self):
+        result = []
+        counter = 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        degree = [0 for _ in range(self.length)]
+        parent = []
+        rank = []
+        for node in range(self.length):
+            parent.append(node)
+            rank.append(0)
+        while len(result) < self.length-1:
+            u, v, w = self.graph[counter]
             counter += 1
+            if (degree[u] > 1) or (degree[v] > 1): 
+                continue
+            
+            x = self.search(parent, u)
+            y = self.search(parent, v)
+            if x != y:
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+                degree[u] += 1
+                degree[v] += 1
+
+        counter = 0
+        final_edge = []
+        for deg in degree:
+            if deg == 1:
+                final_edge.append(counter)
+                if len(final_edge) == 2:
+                    break
+            counter += 1
+        for edge in self.graph:
+            if edge[1] == final_edge[0] and edge[0] == final_edge[1]:
+                result.append(
+                    [edge[1], edge[0], distance( self.cities[edge[1]], self.cities[edge[0]] ) ]
+                    )
+                
+        convert = result.copy()
+        start = convert.pop(0)
+        proper_route = [start[0]]
+        next = start[1]
+        while len(convert) != 0:
+            proper_route.append(next)
+            for edge in convert:
+                if edge[0] == next:
+                    next = edge[1]
+                    convert.remove(edge)
+                    break
+                if edge[1] == next:
+                    next = edge[0]
+                    convert.remove(edge)
+                    break
+        proper_route.append(next)
+
+        return sum([x[2] for x in result]), result, proper_route
  
-    # Update the ending city in array
-    # from city which was last visited
-    route[counter] = 1
- 
-    total += graph[route[counter-1] - 1][0]
-    
-    return total, route
